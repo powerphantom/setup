03c8c08ab5e1718a64e52ee0eb76c9c7d7750fe0
powerphantom <mhughe35@uncc.edu>
2016-04-26 10:21:54 -0400

    Update Setup.h

--- Setup.h
+++ Setup.h
@@ -3,11 +3,12 @@
 #include <stack>
 using namespace std;
 
-class Card {
+class setup {
   private:
     char suit 
     int val;
   public:
     char getsuit(){return suit;};
     char getval(){return val;};
+    void vect_intail();
 };
