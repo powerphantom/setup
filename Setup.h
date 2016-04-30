96bdbbec6797453efbfd54bf8abba24a0cb718ad
powerphantom <mhughe35@uncc.edu>
2016-04-30 13:05:20 -0400

    Update Setup.h

--- Setup.h
+++ Setup.h
@@ -1,14 +1,25 @@
+#ifndef SETUP_H
+#define SETUP_H
+
 #include <vector>
 #include <algorithm>
 #include <stack>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <random>
+
 using namespace std;
 
-class setup {
-  private:
-    char suit 
+struct setup {
+
+    char suit;
     int val;
-  public:
-    char getsuit(){return suit;};
-    char getval(){return val;};
-    void vect_intail();
 };
+setup get_card(char mychar, int myint);
+void vect_intail(vector<setup>& myvect);
+void shuffle_deck(vector<setup>& myvec ,stack<vector<setup>>& mystack);
+void Deal(stack<vector<setup>>& deck, vector<setup>& hand);
+
+#endif
+
