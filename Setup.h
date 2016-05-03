494fe7aaebc5dcf476b1dadb456bcec43b343ba2
powerphantom <mhughe35@uncc.edu>
2016-05-02 21:56:45 -0400

    update

--- Setup.h
+++ Setup.h
@@ -24,6 +24,10 @@ void Deal(stack<setup>& deck, vector<setup>& hand);
 void Sequence_Hand(vector<setup>& hand, vector<setup>& stock);
 void Ask_squence(vector<setup>& hand, vector<setup>& stock);
 void Sequence_Stock(vector<setup>& hand, vector<setup>& stock);
+void Draw_Card (stack<setup>& deck,vector<setup>& hand,vector<setup>& discard);
+void Discard(vector<setup>& hand, vector<setup>& discard);
+void Com_Draw(stack<setup>& deck, vector<setup>& com_hand);
+void Com_Discard(vector<setup>& discard, vector<setup>& com_hand);
+void Com_Sequence(vector<setup>& stock, vector<setup>& com_hand);
 
 #endif
-
